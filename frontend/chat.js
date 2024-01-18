88583884c633cea1839d8fa098d38da6ead7c774
joaopsalescarvalho <jp0489214@gmail.com>
2024-01-18 19:15:01 -0300

    Update chat.js

--- frontend/chat.js
+++ frontend/chat.js
@@ -71,7 +71,7 @@ const handleLogin = (event) => {
     user.color = getRandomColor()
     login.style.display = "none";
     chat.style.display = "flex";
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-t8s1.onrender.com")
     websocket.onmessage = processMessage
 }
 
